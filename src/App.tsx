2ef2474109825bd9d1251f896cef36d854ef1176
Taeyoon Heo <hty050216@snu.ac.kr>
2024-09-11 22:47:13 +0900

    profile update

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,39 @@ import solved from './assets/solved_ac.png';
 
 function App() {
   return (
-    <div className='wrapper'>
-      
-      <img src={profileImage} className='profile'/>
-      <div className='introduction'>
+    <div className="wrapper">
+      <img src={profileImage} className="profile" />
+      <div className="introduction">
         <h2>컴퓨터공학부 24학번 허태윤</h2>
-        <br/>
+        <br />
         <ul>
           <li>와플 스튜디오 22.5기 프론트엔드 루키 </li>
           <li>ENFP😆😆</li>
           <li>개발 관련 지식과 팁을 많이 얻어가고 싶어요!</li>
           <li>인스타그램 맞팔로우 환영!!</li>
         </ul>
-        <br/>
-        <div className='logo-wrapper'>
-          <a href='https://github.com/working05' target="_blank" rel="noreferrer">
-            <img src={github} className='logo'/>
+        <br />
+        <div className="logo-wrapper">
+          <a
+            href="https://github.com/working05"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} className="logo" />
           </a>
-          <a href='https://solved.ac/profile/working05' target="_blank" rel="noreferrer">
-            <img src={solved} className='logo'/>
+          <a
+            href="https://solved.ac/profile/working05"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={solved} className="logo" />
           </a>
-          <a href='https://www.instagram.com/heo_taeyoon/' target="_blank" rel="noreferrer">
-            <img src={instagram} className='logo'/>
+          <a
+            href="https://www.instagram.com/heo_taeyoon/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagram} className="logo" />
           </a>
         </div>
       </div>
